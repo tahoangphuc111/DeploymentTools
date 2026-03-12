24fa3ae27d0ce0c0fd099ef0b91e98d0cb171c38
tahoangphuc111 <tahoangphuc111@gmail.com>
2026-03-12 12:37:47 +0700

    Modify auto-install.sh for Python 3.10 setup
    Updated the auto-install script to use Python 3.10 and added necessary repositories and packages.

--- VNOJ/sample-config/auto-install.sh
+++ VNOJ/sample-config/auto-install.sh
@@ -1,5 +1,9 @@
 # Installing the prerequisites
 sudo apt update - y
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.10 python3.10-venv python3.10-dev
 sudo apt install -y git gcc g++ make python3-dev python3-pip python3-venv libxml2-dev libxslt1-dev zlib1g-dev gettext curl redis-server pkg-config virtualenv memcached build-essential libseccomp-dev
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
@@ -14,7 +18,7 @@ sudo mysql -e "GRANT ALL PRIVILEGES ON dmoj.* TO 'dmoj'@'localhost' IDENTIFIED B
 mariadb-tzinfo-to-sql /usr/share/zoneinfo | sudo mariadb -u root mysql
 
 # Install with virtual environment
-virtualenv venv
+python3.10 -m venv venv
 . venv/bin/activate
 cd site
 pip3 install mysqlclient==2.1.1
